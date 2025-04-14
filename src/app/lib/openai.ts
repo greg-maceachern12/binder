0ee93541645d8b0531f06c290c40a20da8655f78
Greg MacEachern <gregmaceachern98@gmail.com>
2025-04-14 15:44:59 -0400

    Update openai.ts

--- src/app/lib/openai.ts
+++ src/app/lib/openai.ts
@@ -4,8 +4,8 @@ if (!process.env.OPENROUTER_API_KEY) {
   throw new Error('Missing API Key');
 }
 
-export const aiModelSyllabus = "openai/gpt-4o-mini";
-export const aiModelLesson = "google/gemini-flash-1.5";
+export const aiModelSyllabus = "openai/gpt-4.1-mini";
+export const aiModelLesson = "openai/gpt-4.1";
 
 export const openai = new OpenAI({
     // apiKey: process.env.OPENAI_API_KEY
